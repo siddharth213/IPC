b3963ec5583d9dbdd7cdf13f77953e1748c141dc
Chandrakala <62230728+Chandrakala-chandu@users.noreply.github.com>
2020-05-04 22:27:06 +0530

    Header file
    Tell-tale lamps header file

---
+++ IPC/Core/Inc/telltales.h
@@ -0,0 +1,23 @@
+/*
+* telltales.h 
+*
+* Created on: May 4, 2020
+* Author : Kala
+*/
+
+#include"main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#ifndef TELLTALES_H_
+#define TELLTALES_H_
+
+
+
+void seatBelt_status(void);
+void handBrake_status(void);
+int generateRandom_nums(int,int);
+void fuelLevel_status(void);
+
+#endif 
