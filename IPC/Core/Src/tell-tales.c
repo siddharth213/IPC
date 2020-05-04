093fb41c4a196b6cbe1c7f449ccde59a0cb0a53b
Chandrakala <62230728+Chandrakala-chandu@users.noreply.github.com>
2020-05-04 22:22:03 +0530

    Update tell-tales.c

--- IPC/Core/Src/tell-tales.c
+++ IPC/Core/Src/tell-tales.c
@@ -1,33 +1,17 @@
-
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : tell-tales.c
-  * @brief          : program to display the tell-tale lamps of Instrument Panel Cluster
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under Ultimate Liberty license
-  * SLA0044, the "License"; You may not use this file except in compliance with
-  * the License. You may obtain a copy of the License at:
-  *                             www.st.com/SLA0044
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-
-/* Includes ---------------------------------------------------------/
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 /*
- * seatBelt_status reads the specified pin, compares the value and accordingly
- * 		the output value is set/reset on the specified pin
+  * telltales.c
+  * Created on:  April 30,2020
+  * Author : Kala
   */
+/*
+ **************************************************************************************************
+ * @fn              : seatBelt_status
+ * @brief           : read the specified pin, compares the value and accordingly the output value 
+ *                    is set/reset on the specified pin
+ * @param           : None
+ * @return          : None
+ **************************************************************************************************
+*/
 void seatBelt_status(void)
 {
 	if(HAL_GPIO_ReadPin(GPIOC,seatBeltSts_Pin) == 0)
@@ -40,8 +24,15 @@ void seatBelt_status(void)
 	}
 }
 
-/* handBrake_status read the specified pin, compares the value and accordingly
- * 		 the output value is set/reset on the specified pin*/
+/*
+ *************************************************************************************************
+ * @fn               : handBrake_status
+ * @breif            : read the specified pin, compares the value and accordingly the output value
+ *                     is set/reset on the specified pin
+ * @param            : None
+ * @return           : None
+ **************************************************************************************************
+ */
 void handBrake_status(void)
 {
 	if(HAL_GPIO_ReadPin(GPIOC,handBrakeSts_Pin) == 0)
@@ -53,7 +44,15 @@ void handBrake_status(void)
 	HAL_GPIO_WritePin(GPIOC,handBrake_LED_Pin, GPIO_PIN_RESET);
 	}
 }
-
+/*
+***************************************************************************************************
+* @fn                 : generateRandom_nums
+* @breif              : It is used to generate random numbers
+* @param              : lower    specifies the lowest value to the function
+* @param              : upper    specifies the highest value to the function
+* @return             : None
+**************************************************************************************************
+*/
 int generateRandom_nums(int lower, int upper)
 {
        unsigned int num =0;
@@ -61,7 +60,15 @@ int generateRandom_nums(int lower, int upper)
        // printf("%d ", num);
         return num;
 }
-/* fuelLevel_status gives the output to PWM pin based on the input level of fuel*/
+
+/*
+**************************************************************************************************
+* @fn                   : fuelLevel_status 
+* @brief                : outputs the value to PWM pin based on the input level of fuel
+* @param                : None
+* @return               : None
+**************************************************************************************************
+*/
 void fuelLevel_status(void)
 {
 /*  0 to 200  --> Empty
