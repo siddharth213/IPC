d95504dd704ebc1bcdb52e11e166d4cf325bdbaa
meesalarajesh <62230891+meesalarajesh@users.noreply.github.com>
2020-05-06 15:46:23 +0530

    Update speed.c

--- IPC/Core/Src/speed.c
+++ IPC/Core/Src/speed.c
@@ -1,5 +1,11 @@
 
 #include "speedometer.h"
+
+
+uint16_t g_tripOneCount=0;
+uint16_t g_tripTwoCount=0;
+
+
 void speed()
 {
   
@@ -9,12 +15,47 @@ void speed()
   uint16_t countVal = 0;
   uint16_t odometerCount = 0;
   
+  
+   uint16_t tripOneMeter=0;
+  uint16_t tripTwoMeter=0;
+  
+  uint16_t trip_1_switch=0;
+  uint16_t trip_2_switch=0;
+  
+  
   for(tempVar = 0; tempVar <= rpm; tempVar++)
   {
     countVal = countVal + 1;
     if(countVal == 43)  
     {
       odometerCount = odometerCount + 1;
+        tripOneMeter = tripOneMeter + 1;
+      tripTwoMeter = tripTwoMeter + 1;
+      
+      if((trip_1_switch == 1 || tripOneMeter == 99))
+      {
+          
+          if(tripOneMeter==99)
+          {
+             g_tripOneCount = tripOneMeter;
+          }
+          tripOneMeter =0;
+      }
+      
+      
+       if((trip_2_switch == 1 || tripTwoMeter == 999))
+      {
+          
+          if(tripTwoMeter == 999)
+          {
+             g_tripTwoCount = tripTwoMeter;
+          }
+          
+          tripTwoMeter =0;
+      }
+    
+     
+      
       countVal = 0;
     }
   }
