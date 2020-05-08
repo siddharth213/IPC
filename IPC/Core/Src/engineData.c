010f3c07c4bfb5919461bfb2840ecc0d8eb348dd
sidhu <sidhu@sidhu-Lenovo-Z50-70>
2020-05-08 14:33:50 +0530

    Engine Temperature

--- IPC/Core/Src/engineData.c
+++ IPC/Core/Src/engineData.c
@@ -15,7 +15,7 @@ void getenginedata()
 {
 	rx_data();
 
-	    uint16_t msg[2] = {0};
+	    uint16_t msg[8] = {0};
 	    uint8_t engine_fault_sts = 0;
 	    uint8_t rawTemp = 0;
 	    double engineTemp = 0;
@@ -33,23 +33,25 @@ void getenginedata()
         /*Engine Temperature Decode*/
 	    msg[2] = (((uint16_t)enginedata.data_pkt[2]) & 0x000f) << 8;
 	    msg[3] = (((uint16_t)enginedata.data_pkt[3]) & 0x00FF);
-	    rawTemp = msg[2] | msg[3];
+	    rawTemp = msg[2] | msg[3]; //ADC value of temperature sensor
 
-	    engineTemp = (rawTemp * 3300) / 0xfff;
-	    engineTemp /= 1000.0;
+	    engineTemp = (rawTemp * 3300) / 4095;//reading in mv for 12 bit ADC
+	    engineTemp /= 1000.0;//Reading in V
 	    engineTemp -= 0.760; // Subtract the reference voltage at 25°C
 	    engineTemp /= .0025; // Divide by slope 2.5mV
 	    engineTemp += 25.0; // Add the 25°C
 
 
+	    msg[3] = (((uint16_t)enginedata.data_pkt[4]) & 0x00FF);
 
+	    engine_fault_sts = msg[4] & 0xff; // 0 - No Fault, 0xFF - Fault occured
 }
 
 /*This function encodes engine data in to CAN TX data buffer and it is coming from ECM*/
 void rx_data()
 {
-	uint16_t rpm_value = ((rand () % 6000) + 1);
-	uint16_t engine_temp = ((rand () % 4095) + 1);
+	uint16_t rpm_value = ((rand () % 6000) + 1);//rpm random value
+	uint16_t engine_temp = ((rand () % 4095) + 1);//engine temperature ADC random value
 
 
 	enginedata.data_pkt[0] = ((rpm_value >> 8) & 0x1f); //LSB Engine RPM
