aea30b7ec9b230ac8b838b01a39e2ca3936bff6e
meesalarajesh <62230891+meesalarajesh@users.noreply.github.com>
2020-05-08 15:34:05 +0530

    Update Battery.c

--- IPC/Core/Src/Battery.c
+++ IPC/Core/Src/Battery.c
@@ -1,1 +1,46 @@
-#include <stdio.h>
+
+
+int main()
+{
+
+  /* 0 to 255 --> 0%
+     256 to 1023 --> 25%
+     1024 to 2047 --> 50%
+     2048 to 3071 --> 75%
+     3072 to 4095 -->100%  */
+  
+  uint16_t batteryStatus[5] = {0,25,50,75,100};
+  
+  uint16_t batteryPercentage = 0;
+  
+  
+  while(1)
+  {
+  
+  if(( adcValue > 0 ) && ( adcValue <= 255 ))
+  {
+  batteryPercentage = batteryStatus[0];
+  }
+  
+   else if(( adcValue >= 256 ) && ( adcValue <= 1023 ))
+  {
+  batteryPercentage = batteryStatus[1];
+  }
+  
+   else if(( adcValue >= 1024 ) && ( adcValue <= 2047 ))
+  {
+  batteryPercentage = batteryStatus[2];
+  }
+  
+   else if(( adcValue > 2048 ) && ( adcValue <= 3071 ))
+  {
+  batteryPercentage = batteryStatus[3];
+  }
+  
+   else if(( adcValue > 3072 ) && ( adcValue <= 4096 ))
+  {
+  batteryPercentage = batteryStatus[4];
+  }
+  }
+
+}
